ab313dadf32178968535d3fb0909fa0923c35c79
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2021-04-01 20:24:35 +0200

    Add black/white lists for domain names

--- checks.go
+++ checks.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bufio"
+	"log"
 	"net"
+	"os"
 	"strings"
 )
 
-var unrouteables [15]*net.IPNet
+var (
+	unrouteables [15]*net.IPNet
+	blackhole    []net.IP
+	blacklist    []string
+	whitelist    []string
+)
 
 func initCheck() {
 	cidrstrings := [15]string{
@@ -26,9 +34,61 @@ func initCheck() {
 		"240.0.0.0/4",
 	}
 
+	blackholestrings := []string{
+		"35.201.103.212",
+		"139.45.196.145",
+		"139.45.196.206",
+		"192.243.59.12",
+		"192.243.59.13",
+		"192.243.59.20",
+		"216.21.13.14",
+		"216.21.13.15",
+	}
+
 	for i := range cidrstrings {
 		_, unrouteables[i], _ = net.ParseCIDR(cidrstrings[i])
 	}
+
+	for i := range blackholestrings {
+		blackhole = append(blackhole, net.ParseIP(blackholestrings[i]))
+	}
+
+	blacklist = populateCheck("/etc/blocked.names")
+	whitelist = populateCheck("/etc/allowed.names")
+
+}
+
+func populateCheck(conf string) []string {
+	var dNames []string
+
+	buf, err := os.Open(conf)
+	if err != nil {
+		log.Printf("Error opening file %s", conf)
+		return dNames
+	}
+
+	defer func() {
+		if err = buf.Close(); err != nil {
+			log.Printf("Error closing file %s : %s", conf, err.Error())
+		}
+	}()
+
+	snl := bufio.NewScanner(buf)
+	for snl.Scan() {
+
+		if err := snl.Err(); err == nil {
+			txt := snl.Text()
+			if !strings.HasPrefix(txt, "#") && txt != "" {
+				dNames = append(dNames, txt)
+			} else {
+				log.Printf("Error reading newline in file %s : %s", conf, err.Error())
+				break
+			}
+		}
+
+	}
+
+	return dNames
 }
 
 func checkDomain(domain string) bool {
