5e259a58a74ec2e75cdd22ee274c3c2c10becd45
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2021-04-23 18:56:09 +0200

    Add express analytics (2nd lvl, sort by count)

--- analytics.go
+++ analytics.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sort"
+	"strings"
 )
 
 type Hits struct {
@@ -11,7 +13,32 @@ type Hits struct {
 	tls  uint
 }
 
-var analytics = map[string]Hits{}
+type Express struct {
+	domain  string
+	counter uint
+}
+
+var (
+	analytics = map[string]Hits{}
+	express   = map[string]uint{}
+)
+
+func IncExpress(dName string) {
+	split := strings.Split(dName, ".")
+	var truncated string
+	if len(split) > 1 {
+		truncated = split[len(split)-2] + "." + split[len(split)-1]
+	} else {
+		truncated = split[len(split)-1]
+	}
+
+	_, exist := express[truncated]
+	if exist {
+		express[truncated]++
+	} else {
+		express[truncated] = 1
+	}
+}
 
 func IncDNS(dName string) {
 	hit, exist := analytics[dName]
@@ -31,6 +58,7 @@ func IncHTTP(dName string) {
 	} else {
 		analytics[dName] = Hits{dns: 0, http: 1, tls: 0}
 	}
+	IncExpress(dName)
 }
 
 func IncTLS(dName string) {
@@ -41,6 +69,7 @@ func IncTLS(dName string) {
 	} else {
 		analytics[dName] = Hits{dns: 0, http: 0, tls: 1}
 	}
+	IncExpress(dName)
 }
 
 func handleAnalytics(w http.ResponseWriter, r *http.Request) {
@@ -48,7 +77,7 @@ func handleAnalytics(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprint(w, "<html><head><title>Proxy analytics</title></head><body><table>"+
+	fmt.Fprint(w, "<html><head><title>Proxy analytics</title></head><body><table style='float: left;'>"+
 		"<thead><th>fqdn</th><th>dns</th><th>http</th><th>tls</th><th>total</th></thead>"+
 		"<tbody>")
 	for key := range analytics {
@@ -56,5 +85,20 @@ func handleAnalytics(w http.ResponseWriter, r *http.Request) {
 			key, analytics[key].dns, analytics[key].http, analytics[key].tls,
 			analytics[key].dns+analytics[key].http+analytics[key].tls)
 	}
+	fmt.Fprint(w, "</tbody></table>")
+
+	slice := []Express{}
+	for key := range express {
+		slice = append(slice, Express{domain: key, counter: express[key]})
+	}
+	sort.Slice(slice, func(i int, j int) bool {
+		return slice[i].counter > slice[j].counter
+	})
+
+	fmt.Fprint(w, "<table style='float: right;'><thead><th>domain</th><th>count</th></thead><tbody>")
+	for i := range slice {
+		fmt.Fprintf(w, "<tr><td>%s</td><td>%d</td></tr>", slice[i].domain, slice[i].counter)
+	}
 	fmt.Fprint(w, "</tbody></table></html>")
+
 }
