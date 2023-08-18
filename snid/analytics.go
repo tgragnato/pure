a9e9d7b50fc5db5450f138fd535bb58d24e6f1cd
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2023-08-18 16:27:32 +0200

    Update snid analytics
    Introduce DNS preloading

--- snid/analytics.go
+++ snid/analytics.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"net"
+	"sort"
 	"sync"
 	"time"
 )
@@ -12,6 +14,13 @@ type Analytics struct {
 	sync.Mutex
 	data map[string]uint
 }
+
+type Domain struct {
+	domain  string
+	counter uint
+}
+
+type DomainList []Domain
 
 func (a *Analytics) Inc(dName string) {
 	a.Lock()
@@ -29,7 +38,31 @@ func (a *Analytics) Report() {
 		select {
 		case <-time.NewTicker(time.Hour).C:
 			a.Lock()
-			log.Println(analytics.data)
+			dl := make(DomainList, len(a.data))
+			i := 0
+			for domain, counter := range a.data {
+				dl[i] = Domain{domain, counter}
+				i++
+			}
+			a.Unlock()
+			sort.Slice(dl, func(i, j int) bool {
+				return dl[i].counter < dl[j].counter
+			})
+			for _, orderedLogItem := range dl {
+				log.Printf("%s, %d\n", orderedLogItem.domain, orderedLogItem.counter)
+			}
+		}
+	}
+}
+
+func (a *Analytics) PreloadDNS() {
+	for {
+		select {
+		case <-time.NewTicker(15 * time.Minute).C:
+			a.Lock()
+			for domain := range a.data {
+				go net.LookupIP(domain)
+			}
 			a.Unlock()
 		}
 	}
