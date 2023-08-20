1895b518866837babdd9ca379623df63a98d6528
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2023-08-20 21:54:06 +0200

    Update the firewall and base roles

--- shshd/http.go
+++ shshd/http.go
@@ -6,28 +6,33 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 )
 
-var httpclient = &http.Client{
-	Transport: &http.Transport{
-		DialContext: (&net.Dialer{
-			Timeout:   5 * time.Minute,
-			KeepAlive: time.Millisecond,
-			DualStack: true,
-		}).DialContext,
-		ForceAttemptHTTP2:     false,
-		MaxIdleConnsPerHost:   10,
-		MaxConnsPerHost:       20,
-		IdleConnTimeout:       5 * time.Minute,
-		ResponseHeaderTimeout: 2 * time.Second,
-	},
-	Timeout: 5 * time.Minute,
-	CheckRedirect: func(*http.Request, []*http.Request) error {
-		return http.ErrUseLastResponse
-	},
-}
+var (
+	proxy, _   = url.Parse("socks5://[::1]:9050")
+	httpclient = &http.Client{
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout:   5 * time.Minute,
+				KeepAlive: time.Millisecond,
+				DualStack: true,
+			}).DialContext,
+			ForceAttemptHTTP2:     false,
+			MaxIdleConnsPerHost:   10,
+			MaxConnsPerHost:       20,
+			IdleConnTimeout:       5 * time.Minute,
+			ResponseHeaderTimeout: 2 * time.Second,
+			Proxy:                 http.ProxyURL(proxy),
+		},
+		Timeout: 5 * time.Minute,
+		CheckRedirect: func(*http.Request, []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+)
 
 func handleHTTPForward(w http.ResponseWriter, r *http.Request) {
 	host := strings.TrimSuffix(r.Host, ":80")
@@ -37,15 +42,25 @@ func handleHTTPForward(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	is_apple := r.Host == "gs.apple.com"
-	is_updates := r.Host == "updates-http.cdn-apple.com"
-	if !is_apple && !is_updates {
+	switch host {
+	case "updates-http.cdn-apple.com", "gs.apple.com":
+		r.URL.Scheme = "http"
+	case "ocsp.digicert.com", "r3.o.lencr.org", "ocsp2.globalsign.com", "ocsp.sectigo.com", "ocsp.usertrust.com":
+		r.URL.Scheme = "http"
+		r.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0")
+		r.Header.Del("X-Apple-Request-UUID")
+	case "static.ess.apple.com", "certs.apple.com":
+		r.URL.Scheme = "https"
+	case "ocsp.pki.goog", "proxy-safebrowsing.googleapis.com", "ocsp.r2m01.amazontrust.com", "ocsp.rootca1.amazontrust.com":
+		r.URL.Scheme = "https"
+		r.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/116.0")
+		r.Header.Del("X-Apple-Request-UUID")
+	default:
 		http.Redirect(w, r, "https://"+host+r.URL.RequestURI(), http.StatusMovedPermanently)
 		return
 	}
 
-	r.URL.Scheme = "http"
-	r.URL.Host = host
+	r.URL.Host = r.Host
 	r.RequestURI = ""
 
 	resp, err := httpclient.Do(r)
