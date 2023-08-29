30fa30da2dd2af9431963ae3269a693fe8f7735d
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2023-08-29 09:15:48 +0200

    Implement the DNS Type65 (svcb for https)

--- dnsd/main.go
+++ dnsd/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"net"
 
 	"github.com/miekg/dns"
 )
 
 func main() {
+
+	hint4 := ""
+	hint6 := ""
+	flag.StringVar(&hint4, "hintIPv4", "", "Set here the IPv4 of the HTTPS hint")
+	flag.StringVar(&hint6, "hintIPv6", "", "Set here the IPv6 of the HTTPS hint")
+	flag.Parse()
+
+	if hint4 == "" || hint6 == "" {
+		log.Fatalln("Failed to acquire hints")
+	}
+
+	hintIPv4 = net.ParseIP(hint4).To4()
+	if hintIPv4 == nil {
+		log.Fatalln("Failed to parse IPv4")
+	}
+
+	hintIPv6 = net.ParseIP(hint6)
+	if hintIPv6 == nil {
+		log.Fatalln("Failed to parse IPv6")
+	}
 
 	dns.HandleFunc(".", handleDnsRequest)
 	initCheck()
@@ -18,7 +40,7 @@ func main() {
 		}
 		err := udp.ListenAndServe()
 		if err != nil {
-			log.Printf("Failed to start server: %s\n", err.Error())
+			log.Fatalf("Failed to start server: %s\n", err.Error())
 		} else {
 			defer udp.Shutdown()
 		}
@@ -30,7 +52,7 @@ func main() {
 	}
 	err := tcp.ListenAndServe()
 	if err != nil {
-		log.Printf("Failed to start server: %s\n", err.Error())
+		log.Fatalf("Failed to start server: %s\n", err.Error())
 	} else {
 		defer tcp.Shutdown()
 	}
