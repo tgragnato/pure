6fa6b7e6855f9657179471717a1e56f415893671
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2023-10-01 23:38:45 +0200

    feat(ansible): route via haproxy

--- shshd/main.go
+++ shshd/main.go
@@ -1,32 +1,42 @@
 package main
 
 import (
-	"flag"
 	"log"
+	"net"
 	"net/http"
+	"net/url"
+	"time"
+)
+
+var (
+	proxy, _   = url.Parse("socks5://[::1]:9050")
+	httpclient = &http.Client{
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout:   5 * time.Minute,
+				KeepAlive: time.Millisecond,
+				DualStack: true,
+			}).DialContext,
+			ForceAttemptHTTP2:     false,
+			MaxIdleConnsPerHost:   10,
+			MaxConnsPerHost:       20,
+			IdleConnTimeout:       5 * time.Minute,
+			ResponseHeaderTimeout: 2 * time.Second,
+			Proxy:                 http.ProxyURL(proxy),
+		},
+		Timeout: 5 * time.Minute,
+		CheckRedirect: func(*http.Request, []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
 )
 
 func main() {
 
-	iface4 := ""
-	iface6 := ""
-	flag.StringVar(&iface4, "interfaceIPv4", "", "Set here the IPv4 of the interface to bind to")
-	flag.StringVar(&iface6, "interfaceIPv6", "", "Set here the IPv6 of the interface to bind to")
-	flag.Parse()
-
 	handler := http.DefaultServeMux
 	handler.HandleFunc("/", handleHTTPForward)
 
-	if iface6 != "" {
-		go func() {
-			err := http.ListenAndServe(iface6+":80", handler)
-			if err != nil {
-				log.Fatalf("Failed to start server: %s\n", err.Error())
-			}
-		}()
-	}
-
-	err := http.ListenAndServe(iface4+":80", handler)
+	err := http.ListenAndServe("[::1]:80", handler)
 	if err != nil {
 		log.Fatalf("Failed to start server: %s\n", err.Error())
 	}
--- snid/main.go
+++ snid/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net"
 )
 
 func main() {
-
-	iface4 := ""
-	iface6 := ""
-	flag.StringVar(&iface4, "interfaceIPv4", "", "Set here the IPv4 of the interface to bind to")
-	flag.StringVar(&iface6, "interfaceIPv6", "", "Set here the IPv6 of the interface to bind to")
-	flag.Parse()
 
 	if errGeo != nil {
 		log.Fatalf("Failed to read the geoIP database: %s\n", errGeo.Error())
@@ -20,23 +13,7 @@ func main() {
 
 	go analytics.Report()
 
-	if iface6 != "" {
-		go func() {
-			listener, err := net.Listen("tcp6", iface6+":443")
-			if err != nil {
-				log.Fatalf("Failed to start server: %s\n", err.Error())
-			}
-			for {
-				flow, err := listener.Accept()
-				if err != nil {
-					continue
-				}
-				go EstablishFlow(flow)
-			}
-		}()
-	}
-
-	listener, err := net.Listen("tcp4", iface4+":443")
+	listener, err := net.Listen("tcp6", "[::1]:443")
 	if err != nil {
 		log.Fatalf("Failed to start server: %s\n", err.Error())
 	}
