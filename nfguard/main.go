ce1dc9cc54367b6a929994f24021504702c4cc5d
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2023-11-15 13:34:41 +0100

    Extend the application of nfqueue techniques to additional traffic

--- nfguard/main.go
+++ nfguard/main.go
@@ -5,24 +5,31 @@ import (
 	"sync"
 )
 
-var (
-	windowSizeMin uint
-	windowSizeMax uint
-)
-
 func main() {
 
-	var queueNum int
+	var (
+		queueNum      int
+		windowSizeMin uint
+		windowSizeMax uint
+	)
 
 	flag.IntVar(&queueNum, "queueNum", 8, "The number of NFQUEUEs to attach to")
 	flag.UintVar(&windowSizeMin, "windowSizeMin", 60, "Minimum TCP Window")
 	flag.UintVar(&windowSizeMax, "windowSizeMax", 90, "Maximum TCP Window")
 	flag.Parse()
 
+	go analytics.Report()
+
 	var wg sync.WaitGroup
 	for i := 0; i < queueNum; i++ {
 		wg.Add(1)
-		go queueWorker(uint16(i), i >= queueNum/2, &wg)
+		go queueWorker(
+			uint16(i),
+			windowSizeMin,
+			windowSizeMax,
+			i >= queueNum/2,
+			&wg,
+		)
 	}
 	wg.Wait()
 }
