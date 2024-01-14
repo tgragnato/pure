27b5d562420fb55db81c4ad1bae906efc4470c4e
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-01-14 19:30:23 +0100

    feat(dnsd): introduce an allowlist

---
+++ pkg/checks/domain.go
@@ -0,0 +1,26 @@
+package checks
+
+import "strings"
+
+var whitelist = []string{
+	"d3w4wp6rol9nvz.cloudfront.net.",
+	"gcs-eu-00002.content-storage-download.googleapis.com.",
+	"gcs-eu-00002.content-storage-upload.googleapis.com.",
+}
+
+func CheckDomain(domain string) bool {
+
+	for _, allowed := range whitelist {
+		if domain == allowed {
+			return true
+		}
+	}
+
+	for _, suffix := range blacklist {
+		if strings.HasSuffix(domain, suffix) {
+			return false
+		}
+	}
+
+	return true
+}
