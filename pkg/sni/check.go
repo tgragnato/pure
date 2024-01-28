a00e685e9b7a6d47f5193b34e5a2a4ef288a7d73
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-01-28 18:56:05 +0100

    chore(nfguard): update check.go

--- pkg/sni/check.go
+++ pkg/sni/check.go
@@ -2,6 +2,7 @@ package sni
 
 import (
 	"net"
+	"strings"
 )
 
 func checkDomain(domain string) bool {
@@ -21,4 +22,12 @@ func checkDomain(domain string) bool {
 	}
 
 	return true
+}
+
+func getHostPort(sni string) string {
+	if strings.HasSuffix(sni, "imap.mail.me.com") || sni == "imap.gmail.com" {
+		return net.JoinHostPort(sni, "993")
+	}
+
+	return net.JoinHostPort(sni, "443")
 }
