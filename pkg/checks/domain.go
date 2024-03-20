19aec22de9bc87007bd55ac83b349e4c9f1b240f
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-03-20 22:35:38 +0100

    chore(dnsd): update domain.go

--- pkg/checks/domain.go
+++ pkg/checks/domain.go
@@ -4,11 +4,10 @@ import "strings"
 
 var whitelist = []string{
 	"apple-finance.query.yahoo.com.",
-	"d3w4wp6rol9nvz.cloudfront.net.",
 	"dnfed9a4ru7oh.cloudfront.net.",
+	"d1612au5bhln8.cloudfront.net.",
 	"gcs-eu-00002.content-storage-download.googleapis.com.",
 	"gcs-eu-00002.content-storage-upload.googleapis.com.",
-	"mattermost.pepita.io.",
 }
 
 func CheckDomain(domain string) bool {
