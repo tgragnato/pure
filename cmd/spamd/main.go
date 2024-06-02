ae9ec0d4b381ef785dcb103b93573c6ad8255d0f
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-06-02 16:47:04 +0200

    chore(spamd): update main.go

--- cmd/spamd/main.go
+++ cmd/spamd/main.go
@@ -23,60 +23,127 @@ var (
 			DualStack: true,
 		}).DialContext,
 		ForceAttemptHTTP2:     true,
-		MaxIdleConns:          100,
-		MaxIdleConnsPerHost:   100,
-		MaxConnsPerHost:       100,
-		IdleConnTimeout:       time.Hour,
+		MaxIdleConns:          4,
+		MaxIdleConnsPerHost:   4,
+		MaxConnsPerHost:       4,
+		IdleConnTimeout:       time.Minute,
 		TLSHandshakeTimeout:   time.Second,
 		ExpectContinueTimeout: time.Second,
 		ResponseHeaderTimeout: time.Second,
 		DisableKeepAlives:     false,
 	}}
-	numberOfWorkers = 10
-	urls            = []string{
-		"https://tinyurl.com/HIMALA12/4GbkAE80702KUYR767luahfovgfo8171TSOJJDIWIOPSUFW13839/18625t22#txzrkskcjttglrmclxwfu",
-		"http://linioki.com/6Chwaz80702KKGq767hgugxsgkfh8171DAMTTNPHCPQMXUF13839/18625h22",
-		"https://www.httpsimage.com/v2/03d8266e-9bcd-49b3-b11f-57360eafbe31.png",
-		"https://tinyurl.com/HIMALA12/5eYbZo80702JGjN767ypgmbwniii8171AWRGFKLKLRPKLPM13839/18625X22#gypdyeghnejaegotzgqyt",
-		"http://linioki.com/5eYbZo80702JGjN767ypgmbwniii8171AWRGFKLKLRPKLPM13839/18625X22#gypdyeghnejaegotzgqyt",
-		"https://tinyurl.com/HIMALA12/6Chwaz80702KKGq767hgugxsgkfh8171DAMTTNPHCPQMXUF13839/18625h22",
-		"http://linioki.com/6Chwaz80702KKGq767hgugxsgkfh8171DAMTTNPHCPQMXUF13839/18625h22",
-		"https://tinyurl.com/HIMALA12/track/3TMvbV80702vdqw767brbxkphbsk8171SFFDKUUQMREBQPG13839/18625p22",
-		"http://linioki.com/track/3TMvbV80702vdqw767brbxkphbsk8171SFFDKUUQMREBQPG13839/18625p22",
-		"https://tinyurl.com/HIMALA12/4WKKpm80770NJNV1619ryqbstiovr8171MWJCRLTEIYFSZON13839/18625R22#lhsglrclhqluewhayihdy",
-		"http://linioki.com/4WKKpm80770NJNV1619ryqbstiovr8171MWJCRLTEIYFSZON13839/18625R22#lhsglrclhqluewhayihdy",
-		"https://tinyurl.com/HIMALA12/4awNPL80770yTrT1619tgnqwmdjpt8171AAXOREBDUDUWDAA13839/18625g22#nqrnyfewonnhvgrizlsjz",
-		"http://linioki.com/4upvoz80770TqYK1619fcsahliicq8171PCLOYQTVNNOSXJT13839/18625S22#drsbyizemphcffpuxqwje",
-		"https://tinyurl.com/HIMALA12/4upvoz80770TqYK1619fcsahliicq8171PCLOYQTVNNOSXJT13839/18625S22#drsbyizemphcffpuxqwje",
-		"http://linioki.com/4upvoz80770TqYK1619fcsahliicq8171PCLOYQTVNNOSXJT13839/18625S22#drsbyizemphcffpuxqwje",
-		"https://tinyurl.com/HIMALA12/5fJPcu80770LrUx1619qorpaodvmu8171ALLOFCDQMYEKMIT13839/18625x22#xqxhaflofarvykfyvweyr",
-		"http://linioki.com/5fJPcu80770LrUx1619qorpaodvmu8171ALLOFCDQMYEKMIT13839/18625x22#xqxhaflofarvykfyvweyr",
-		"https://tinyurl.com/HIMALA12/6jxIcg80770jiBE1619zosbmbjiag8171IJNXYRAQAPYTMBB13839/18625T22",
-		"http://linioki.com/6jxIcg80770jiBE1619zosbmbjiag8171IJNXYRAQAPYTMBB13839/18625T22",
-		"https://tinyurl.com/HIMALA12/4HXjoo80791dYtM1195zuwviakgln8171EJFZXXVQGNPXAOK13839/18625R22#pdbppwwlaexditvthghaj",
-		"http://linioki.com/4HXjoo80791dYtM1195zuwviakgln8171EJFZXXVQGNPXAOK13839/18625R22#pdbppwwlaexditvthghaj",
-		"https://tinyurl.com/HIMALA12/4sCtWa80791GpEG1195teuwrvsxbm8171MYTFABBBOQCNHXM13839/18625v22#riwkuhwealeouqzihzvcb",
-		"http://linioki.com/4sCtWa80791GpEG1195teuwrvsxbm8171MYTFABBBOQCNHXM13839/18625v22#riwkuhwealeouqzihzvcb",
-		"https://tinyurl.com/HIMALA12/5dWwxC80791dfpi1195dkmhnmcxcg8171ASVGXUTOSQPQBUC13839/18625c22#nfocppzyzkhclaigfdedr",
-		"http://linioki.com/5dWwxC80791dfpi1195dkmhnmcxcg8171ASVGXUTOSQPQBUC13839/18625c22#nfocppzyzkhclaigfdedr",
-		"https://tinyurl.com/HIMALA12/6RBIAx80791acSe1195cfqticzrpx8171RUIUVYDAHYVPUIZ13839/18625n22",
-		"http://linioki.com/6RBIAx80791acSe1195cfqticzrpx8171RUIUVYDAHYVPUIZ13839/18625n22",
-		"https://tinyurl.com/htsoni/5TcdFH80792pQrq1362smjculxvav8171LYHBVZZVECVIUNG13839/18625M22#zuylgqjyjiflobmwjfcvk",
-		"http://mazaratnadik.com/5TcdFH80792pQrq1362smjculxvav8171LYHBVZZVECVIUNG13839/18625M22#zuylgqjyjiflobmwjfcvk",
-		"https://tinyurl.com/htsoni/6yfdln80792fpEQ1362mzednzrstj8171YKZXKOVZXAHYVAE13839/18625N22#lqzxddadypshbybofumzp",
-		"http://mazaratnadik.com/6yfdln80792fpEQ1362mzednzrstj8171YKZXKOVZXAHYVAE13839/18625N22#lqzxddadypshbybofumzp",
-		"https://tinyurl.com/HIMALA12/5laNXF80790UzLG767fwqmqfwwsz8171JFPBMGWFWHOFAWE13839/18625r22#fbtzrkfnduqlqqiegsvcg",
-		"http://linioki.com/5laNXF80790UzLG767fwqmqfwwsz8171JFPBMGWFWHOFAWE13839/18625r22#fbtzrkfnduqlqqiegsvcg",
-		"https://tinyurl.com/HIMALA12/6DUEvB80790jvRD767claopcblok8171BPLORCVMSAKIGBA13839/18625i22",
-		"http://linioki.com/6DUEvB80790jvRD767claopcblok8171BPLORCVMSAKIGBA13839/18625i22",
-		"https://tinyurl.com/Loura41r/4uIEqG80984Plej1363mrquqxbpan10521NFBEGXOICSJWGLT2040632/18649e11#uhrmkzosemksdrbqpzucd",
-		"http://madishwm.com/4uIEqG80984Plej1363mrquqxbpan10521NFBEGXOICSJWGLT2040632/18649e11#uhrmkzosemksdrbqpzucd",
-		"https://tinyurl.com/Loura41r/5NvKDQ80984riYX1363umvrmnphby10521FORIIULLATURFLG2040632/18649Z11#pfxglimjeauhwqekshuix",
-		"http://madishwm.com/5NvKDQ80984riYX1363umvrmnphby10521FORIIULLATURFLG2040632/18649Z11#pfxglimjeauhwqekshuix",
-		"https://tinyurl.com/Loura41r/6UPeBZ80984EQhU1363qhpdnzbsew10521NBJBRWGNILCFBEZ2040632/18649i11",
-		"http://madishwm.com/6UPeBZ80984EQhU1363qhpdnzbsew10521NBJBRWGNILCFBEZ2040632/18649i11",
-		"https://tinyurl.com/Loura41r/4Iotjw81001AlPk1363ebsqyzwhvs10521SZXAOEISQDZMGFQ2040632/18649v11#qfhofadsqooaihokwkokk",
-		"http://madishwm.com/4Iotjw81001AlPk1363ebsqyzwhvs10521SZXAOEISQDZMGFQ2040632/18649v11#qfhofadsqooaihokwkokk",
+	urls = []string{
+		"https://egsro.com",
+		"https://egsro.com/_fd",
+		"https://egsro.com/_fd/",
+		"https://egsro.com/_tr",
+		"https://egsro.com/_tr/",
+		"https://egsro.com/_zc",
+		"https://egsro.com/_zc/",
+		"https://egsro.com/",
+		"https://egsro.com/brKcJyirt.js",
+		"https://egsro.com/favicon.ico",
+		"https://egsro.com/robots.txt",
+		"https://tinyurl.com/HIMALA12/",
+		"https://tinyurl.com/htsoni/",
+		"https://tinyurl.com/Loura41r/",
+	}
+	domains = []string{
+		"http://694east.com",
+		"http://abortionclinician.com",
+		"http://adanadaemlakci.com",
+		"http://alohateriyakigrillct.com",
+		"http://amirlabd.com",
+		"http://andyalcoholic.com",
+		"http://apedxroll.com",
+		"http://apexroll.com",
+		"http://aqounoli.com",
+		"http://arthurnascosh.com",
+		"http://av82com.com",
+		"http://bantosas.com",
+		"http://barbaraton.com",
+		"http://biken.site",
+		"http://brikonagio.net",
+		"http://cbdido.com",
+		"http://clmm1.com",
+		"http://confortour.com",
+		"http://cukuinhospital.com",
+		"http://diva138.com",
+		"http://dkholpok.com",
+		"http://doorstoda.com",
+		"http://einfachsuchen.com",
+		"http://escapepodnet.com",
+		"http://fotografwest.com",
+		"http://francegiude.com",
+		"http://handalh.com",
+		"http://ilyion.com",
+		"http://imtoken-app.com",
+		"http://irishwoodwatches.com",
+		"http://jzcai1603.com",
+		"http://kanboris.com",
+		"http://kasvigas.com",
+		"http://kasvigas.net",
+		"http://kolgyh.com",
+		"http://labuiand.com",
+		"http://lagartad.com",
+		"http://lakartpa.com",
+		"http://larabonis.com",
+		"http://latfitx.com",
+		"http://linioki.com",
+		"http://lintosbant.com",
+		"http://madishwm.com",
+		"http://maikodmi.net",
+		"http://malawais.com",
+		"http://malisdakoni.com",
+		"http://mandarroskolo.com",
+		"http://markiisa.com",
+		"http://masandinara.com",
+		"http://maxicomsrl.com",
+		"http://mazaratnadik.com",
+		"http://mzolpon.com",
+		"http://mzyikon.com",
+		"http://oy2order.com",
+		"http://parteekit.com",
+		"http://point-s.net",
+		"http://pucuk69.com",
+		"http://servantidal.com",
+		"http://startkolcas.com",
+		"http://trkoli.com",
+		"http://tuv-nord.net",
+		"http://viohki.com",
+		"http://viokyi.com",
+		"http://waletnas.com",
+		"http://zarbonk.com",
+		"http://zoheoki.com",
+	}
+	paths = []string{
+		"",
+		"/",
+		"/4HXjoo80791dYtM1195zuwviakgln8171EJFZXXVQGNPXAOK13839/18625R22#pdbppwwlaexditvthghaj",
+		"/4Iotjw81001AlPk1363ebsqyzwhvs10521SZXAOEISQDZMGFQ2040632/18649v11#qfhofadsqooaihokwkokk",
+		"/4sCtWa80791GpEG1195teuwrvsxbm8171MYTFABBBOQCNHXM13839/18625v22#riwkuhwealeouqzihzvcb",
+		"/4uIEqG80984Plej1363mrquqxbpan10521NFBEGXOICSJWGLT2040632/18649e11#uhrmkzosemksdrbqpzucd",
+		"/4upvoz80770TqYK1619fcsahliicq8171PCLOYQTVNNOSXJT13839/18625S22#drsbyizemphcffpuxqwje",
+		"/4WKKpm80770NJNV1619ryqbstiovr8171MWJCRLTEIYFSZON13839/18625R22#lhsglrclhqluewhayihdy",
+		"/5dWwxC80791dfpi1195dkmhnmcxcg8171ASVGXUTOSQPQBUC13839/18625c22#nfocppzyzkhclaigfdedr",
+		"/5eYbZo80702JGjN767ypgmbwniii8171AWRGFKLKLRPKLPM13839/18625X22#gypdyeghnejaegotzgqyt",
+		"/5fJPcu80770LrUx1619qorpaodvmu8171ALLOFCDQMYEKMIT13839/18625x22#xqxhaflofarvykfyvweyr",
+		"/5laNXF80790UzLG767fwqmqfwwsz8171JFPBMGWFWHOFAWE13839/18625r22#fbtzrkfnduqlqqiegsvcg",
+		"/5NvKDQ80984riYX1363umvrmnphby10521FORIIULLATURFLG2040632/18649Z11#pfxglimjeauhwqekshuix",
+		"/5TcdFH80792pQrq1362smjculxvav8171LYHBVZZVECVIUNG13839/18625M22#zuylgqjyjiflobmwjfcvk",
+		"/6Chwaz80702KKGq767hgugxsgkfh8171DAMTTNPHCPQMXUF13839/18625h22",
+		"/6DUEvB80790jvRD767claopcblok8171BPLORCVMSAKIGBA13839/18625i22",
+		"/6jxIcg80770jiBE1619zosbmbjiag8171IJNXYRAQAPYTMBB13839/18625T22",
+		"/6RBIAx80791acSe1195cfqticzrpx8171RUIUVYDAHYVPUIZ13839/18625n22",
+		"/6UPeBZ80984EQhU1363qhpdnzbsew10521NBJBRWGNILCFBEZ2040632/18649i11",
+		"/6yfdln80792fpEQ1362mzednzrstj8171YKZXKOVZXAHYVAE13839/18625N22#lqzxddadypshbybofumzp",
+		"/about",
+		"/news",
+		"/opt-out",
+		"/privacy",
+		"/robots.txt",
+		"/t/robots.txt",
+		"/track/3TMvbV80702vdqw767brbxkphbsk8171SFFDKUUQMREBQPG13839/18625p22",
 	}
 	userAgents = []string{
 		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
@@ -86,6 +153,7 @@ var (
 		"Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/125.0.0.0 Safari/537.36",
 		"Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36 (compatible; Google-InspectionTool/1.0;)",
 		"Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36 (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)",
+		"Mozilla/5.0 (Windows NT 10.0; rv:128.0) Gecko/20100101 Firefox/128.0",
 	}
 )
 
@@ -95,28 +163,31 @@ func main() {
 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
 
 	for range time.NewTicker(time.Minute).C {
-		for i := 0; i < numberOfWorkers; i++ {
-			items := make([]string, len(urls))
-			copy(items, urls)
-			rand.Shuffle(len(urls), func(i, j int) {
-				items[i], items[j] = items[j], items[i]
-			})
-			for _, url := range items {
-				go call(url, i)
+		items := make([]string, len(urls))
+		copy(items, urls)
+		for _, domain := range domains {
+			for _, path := range paths {
+				items = append(items, domain+path)
 			}
+		}
+		rand.Shuffle(len(urls), func(i, j int) {
+			items[i], items[j] = items[j], items[i]
+		})
+		for index, url := range items {
+			go call(url, userAgents[index%len(userAgents)])
 		}
 	}
 
 	<-signalCh
 }
 
-func call(url string, i int) {
+func call(url string, userAgent string) {
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	req.Header.Set("User-Agent", userAgents[i%len(userAgents)])
+	req.Header.Set("User-Agent", userAgent)
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		return
