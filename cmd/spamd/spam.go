43bb77ad89cfdb13b3397882c52ff3ffdf4d7474
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-06-24 20:31:56 +0200

    feat(spamd): support a new format

--- cmd/spamd/spam.go
+++ cmd/spamd/spam.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
 	"fmt"
 	"math/rand"
 	"net/http"
+	"text/template"
 )
 
 type spam struct {
@@ -20,59 +23,82 @@ func (s *spam) call() {
 		fmt.Println(err.Error())
 		return
 	}
-	counter++
 	req.Header.Set("User-Agent", s.userAgent)
 	var resp *http.Response
+	counter++
 	if s.proxy {
 		resp, err = httpClient.Do(req)
 	} else {
 		resp, err = directHttp.Do(req)
 	}
+	counter--
 	if err != nil {
-		counter--
 		return
 	}
 	resp.Body.Close()
-	counter--
 }
 
 func (s *spam) random() {
 	characters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	length := rand.Intn(10) + 60
+	length := rand.Intn(10) + 52
 	addition := make([]byte, length)
 	for i := 0; i < length; i++ {
 		addition[i] = characters[rand.Intn(len(characters))]
 	}
 
-	randomUrl := fmt.Sprintf("/%s/%s#%s", addition[:length-30], addition[length-30:length-22], addition[length-22:])
+	randomUrl := fmt.Sprintf("/%s/%s", addition[:length-30], addition[length-30:])
 	if rand.Intn(2) == 1 {
 		s.url += "/track"
 	}
 	s.url += randomUrl
 }
 
+func (s *spam) template() {
+	text := templates[rand.Intn(len(templates))]
+
+	characters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789."
+	length := rand.Intn(20) + 5
+	addition := make([]byte, length)
+	for i := 0; i < length; i++ {
+		addition[i] = characters[rand.Intn(len(characters))]
+	}
+
+	tmpl, err := template.New("template").Parse(text)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	var buf bytes.Buffer
+	tmpl.Execute(&buf, struct{ Padding string }{string(addition)})
+	if len(buf.Bytes()) == 0 {
+		fmt.Println("empty rendered template")
+		return
+	}
+
+	if rand.Intn(2) == 1 {
+		s.url += "?up=" + base64.StdEncoding.EncodeToString(buf.Bytes())
+	} else {
+		s.url += "?upn=" + base64.StdEncoding.EncodeToString(buf.Bytes())
+	}
+	if rand.Intn(2) == 1 {
+		s.url += "&v=0"
+	}
+}
+
 func makeSpam() spam {
-	switch rand.Intn(4) {
+	switch rand.Intn(6) {
 
 	case 0:
 		return spam{
-			url:       domains[rand.Intn(len(domains))] + paths[rand.Intn(len(paths))],
+			url:       proxiedPrefixes[rand.Intn(len(proxiedPrefixes))] + paths[rand.Intn(len(paths))],
 			method:    http.MethodGet,
 			userAgent: userAgents[rand.Intn(len(userAgents))],
 			proxy:     true,
 		}
 
 	case 1:
-		return spam{
-			url:       "http://162.62.230.106" + paths[rand.Intn(len(paths))],
-			method:    http.MethodGet,
-			userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/604.1",
-			proxy:     false,
-		}
-
-	case 2:
 		s := spam{
-			url:       freshDomains[rand.Intn(len(freshDomains))],
+			url:       proxiedPrefixes[rand.Intn(len(proxiedPrefixes))],
 			method:    http.MethodGet,
 			userAgent: userAgents[rand.Intn(len(userAgents))],
 			proxy:     true,
@@ -80,18 +106,42 @@ func makeSpam() spam {
 		s.random()
 		return s
 
-	case 3:
+	case 2:
 		s := spam{
-			url:       freshDomains[rand.Intn(len(freshDomains))],
+			url:       proxiedPrefixes[rand.Intn(len(proxiedPrefixes))],
 			method:    http.MethodGet,
 			userAgent: userAgents[rand.Intn(len(userAgents))],
 			proxy:     true,
 		}
-		if rand.Intn(2) == 1 {
-			s.random()
-		} else {
-			s.url += paths[rand.Intn(len(paths))]
+		s.template()
+		return s
+
+	case 3:
+		return spam{
+			url:       directPrefixes[rand.Intn(len(directPrefixes))] + paths[rand.Intn(len(paths))],
+			method:    http.MethodGet,
+			userAgent: userAgents[0],
+			proxy:     false,
 		}
+
+	case 4:
+		s := spam{
+			url:       directPrefixes[rand.Intn(len(directPrefixes))],
+			method:    http.MethodGet,
+			userAgent: userAgents[0],
+			proxy:     false,
+		}
+		s.random()
+		return s
+
+	case 5:
+		s := spam{
+			url:       directPrefixes[rand.Intn(len(directPrefixes))],
+			method:    http.MethodGet,
+			userAgent: userAgents[0],
+			proxy:     false,
+		}
+		s.template()
 		return s
 	}
 
