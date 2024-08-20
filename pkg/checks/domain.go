4c2e285f259f3b2ececf0dec8ca354ba39e1230f
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-08-20 22:04:55 +0200

    feat(dnsd): add whitelist by suffix

--- pkg/checks/domain.go
+++ pkg/checks/domain.go
@@ -4,7 +4,6 @@ import "strings"
 
 var whitelist = []string{
 	"apple-finance.query.yahoo.com.",
-	"blob.core.windows.net.",
 	"d1612au5bhln8.cloudfront.net.",
 	"d179pbpxvabqy1.cloudfront.net.",
 	"d2gwrxj8kvy81r.cloudfront.net.",
@@ -15,14 +14,25 @@ var whitelist = []string{
 	"dnfed9a4ru7oh.cloudfront.net.",
 	"gcs-eu-00002.content-storage-download.googleapis.com.",
 	"gcs-eu-00002.content-storage-upload.googleapis.com.",
-	"semrush.com.",
-	"store.core.windows.net.",
+}
+
+var whitelistSuffix = []string{
+	".blob.core.windows.net.",
+	".bn9prdstrz04a.store.core.windows.net.",
+	".bn9prdstrz04a.trafficmanager.net.",
+	".semrush.com.",
 }
 
 func CheckDomain(domain string) bool {
 
 	for _, allowed := range whitelist {
 		if domain == allowed {
+			return true
+		}
+	}
+
+	for _, suffix := range whitelistSuffix {
+		if strings.HasSuffix(domain, suffix) {
 			return true
 		}
 	}
