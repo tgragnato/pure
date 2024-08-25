c8520e660f009f2eaa6efd0c212e65e2332f1aff
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-08-25 21:53:47 +0200

    test(nfguard): implement unit testing for the shsh pkg

---
+++ pkg/shsh/handler_test.go
@@ -0,0 +1,70 @@
+package shsh
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleHTTPForward(t *testing.T) {
+	t.Parallel()
+
+	// Assuming there's not a proxy server running in the test environment
+	tests := []struct {
+		host       string
+		path       string
+		expected   string
+		statusCode int
+	}{
+		{
+			host:       "static.ess.apple.com",
+			path:       "/connectivity.txt",
+			expected:   "AV was here!",
+			statusCode: http.StatusOK,
+		},
+		{
+			host:       "updates-http.cdn-apple.com",
+			path:       "/",
+			expected:   "<a href=\"https://updates-http.cdn-apple.com/\">Found</a>.\n\n",
+			statusCode: http.StatusFound,
+		},
+		{
+			host:       "ocsp.digicert.com",
+			path:       "/",
+			expected:   "<a href=\"https://ocsp.digicert.com/\">Found</a>.\n\n",
+			statusCode: http.StatusFound,
+		},
+		{
+			host:       "ocsp.pki.goog",
+			path:       "/",
+			expected:   "<a href=\"https://ocsp.pki.goog/\">Found</a>.\n\n",
+			statusCode: http.StatusFound,
+		},
+		{
+			host:       "www.example.com",
+			path:       "/",
+			expected:   "<a href=\"https://www.example.com/\">Moved Permanently</a>.\n\n",
+			statusCode: http.StatusMovedPermanently,
+		},
+	}
+
+	for _, test := range tests {
+		req, err := http.NewRequest("GET", test.path, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Host = test.host
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(handleHTTPForward)
+		handler.ServeHTTP(rr, req)
+
+		if rr.Code != test.statusCode {
+			t.Errorf("For host %s and path %s, expected status code %d, but got %d", test.host, test.path, test.statusCode, rr.Code)
+		}
+
+		if rr.Body.String() != test.expected {
+			t.Errorf("For host %s and path %s, expected response body %q, but got %q", test.host, test.path, test.expected, rr.Body.String())
+		}
+	}
+}
